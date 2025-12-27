37dc0e655c89945dd689dae7e2870641dc5a8e85
AyoAyoos <nakodayush@gmail.com>
2025-12-28 00:06:58 +0530

    feat: Update navbar data with cookbook route

--- frontend/src/components/layout/Navbar/navbar.data.js
+++ frontend/src/components/layout/Navbar/navbar.data.js
@@ -1,28 +1,30 @@
 export const navItems = [
-    {
-      label: "About",
-      bgColor: "#0D0716",
-      textColor: "#fff",
-      links: [
-        { label: "Company", ariaLabel: "About Company", href: "/company" },
-        { label: "Careers", ariaLabel: "About Careers", href: "/careers" }
-      ]
-    },
-    {
-      label: "Recipes",
-      bgColor: "#170D27",
-      textColor: "#fff",
-      links: [
-        { label: "Featured", ariaLabel: "Featured Recipes", href: "/recipes/featured" },
-        { label: "My Pantry", ariaLabel: "My Pantry", href: "/pantry" }
-      ]
-    },
-    {
-      label: "Contact",
-      bgColor: "#271E37", 
-      textColor: "#fff",
-      links: [
-        { label: "Support", ariaLabel: "Email us", href: "/support" }
-      ]
-    }
-  ];
+  {
+    label: "About",
+    bgColor: "#0D0716",
+    textColor: "#fff",
+    links: [
+      { label: "Company", ariaLabel: "About Company", href: "/company" },
+      { label: "Careers", ariaLabel: "About Careers", href: "/careers" }
+    ]
+  },
+  {
+    label: "Recipes",
+    bgColor: "#170D27",
+    textColor: "#fff",
+    links: [
+      { label: "Featured", ariaLabel: "Featured Recipes", href: "/recipes/featured" },
+      { label: "My Pantry", ariaLabel: "My Pantry", href: "/pantry" },
+      // Added My Cookbook link here
+      { label: "My Cookbook", ariaLabel: "My Cookbook", href: "/cookbook" }
+    ]
+  },
+  {
+    label: "Contact",
+    bgColor: "#271E37", 
+    textColor: "#fff",
+    links: [
+      { label: "Support", ariaLabel: "Email us", href: "/support" }
+    ]
+  }
+];
